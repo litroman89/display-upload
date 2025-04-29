5af837fcddb669666b4a634a161fdc25c3cf0949
Roman Litiahin <roman.litiahin@mediamonks.com>
2025-04-29 15:14:44 +0300

    incremental upload

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Filenames = require('./data/Filenames');
 const fs = require('fs-extra');
 const inquirer = require('inquirer');
 const path = require('path');
-const chalk = require('chalk')
+const chalk = require('chalk');
 
 module.exports = async (options = {}, cli) => {
   const filepathRc = `./${Filenames.RC}`;
@@ -18,7 +18,7 @@ module.exports = async (options = {}, cli) => {
 
   // checking if .gitignore is exists
   if (!hasGitIgnore) {
-    const {shouldCreateGitIgnore} = await inquirer.prompt({
+    const { shouldCreateGitIgnore } = await inquirer.prompt({
       type: 'confirm',
       name: 'shouldCreateGitIgnore',
       message: 'No .gitignore found should i create it?',
@@ -36,7 +36,7 @@ module.exports = async (options = {}, cli) => {
     const regEx = new RegExp(Filenames.RC, 'gm');
 
     if (!regEx.test(gitIgnoreContent)) {
-      const {shouldAddIt} = await inquirer.prompt({
+      const { shouldAddIt } = await inquirer.prompt({
         type: 'confirm',
         name: 'shouldAddIt',
         message: `No ${Filenames.RC} was added to the ${Filenames.GITIGNORE}, should i add it?`,
@@ -54,9 +54,7 @@ module.exports = async (options = {}, cli) => {
 
     try {
       rc = await fs.readJson(filepathRc);
-    }
-
-    catch (err) {
+    } catch (err) {
       throw new Error('cant read json from .uploadrc, please delete it and try again');
     }
 
@@ -64,49 +62,43 @@ module.exports = async (options = {}, cli) => {
       data = {
         ...rc,
       };
-    }
-
-    else { // uploadrc exists but not in correct new structure
-      if (rc.hasOwnProperty('type')) { // looks like the object follows old structure
+    } else {
+      // uploadrc exists but not in correct new structure
+      if (rc.hasOwnProperty('type')) {
+        // looks like the object follows old structure
         data = {
-          uploadConfigs: [
-            rc
-          ]
-        }
-      }
-
-      else { // no compatible structure found, creating new
+          uploadConfigs: [rc],
+        };
+      } else {
+        // no compatible structure found, creating new
         data = {
-          uploadConfigs: []
-        }
+          uploadConfigs: [],
+        };
       }
     }
-  }
-
-  else {
-    console.log('uploadrc doesnt exist, creating creating new data obj')
+  } else {
+    console.log('uploadrc doesnt exist, creating creating new data obj');
     data = {
-      uploadConfigs: []
-    }
+      uploadConfigs: [],
+    };
   }
 
   const choices = [
-    {name: 'Mediamonks Preview', value: 'mm-preview'},
-    {name: 'Adform', value: 'adform'},
+    { name: 'Mediamonks Preview', value: 'mm-preview' },
+    { name: 'Adform', value: 'adform' },
     // { name: 'Workspace', value: 'workspace' },
-    {name: 'Flashtalking', value: 'flashtalking'},
-    {name: 'Google DoubleClick Studio', value: 'doubleclick'},
-    {name: 'SFTP (alpha)', value: 'sftp'},
+    { name: 'Flashtalking', value: 'flashtalking' },
+    { name: 'Google DoubleClick Studio', value: 'doubleclick' },
+    { name: 'SFTP (alpha)', value: 'sftp' },
     // { name: 'Amazon S3', value: 's3', disabled: true },
     // { name: 'FTP', value: 'ftp', disabled: true },
     // { name: 'Netflix Monet', value: 'monet', disabled: true },
-
   ];
 
-  data.uploadConfigs.forEach(config => {
-    const configIndex = choices.findIndex(choice => config.type === choice.value);
+  data.uploadConfigs.forEach((config) => {
+    const configIndex = choices.findIndex((choice) => config.type === choice.value);
     if (configIndex !== -1) choices[configIndex].name += ' (Config Found)';
-  })
+  });
 
   uploadTarget = await conditionalPrompt(options, {
     type: 'list',
@@ -121,14 +113,14 @@ module.exports = async (options = {}, cli) => {
     throw new Error(`unknown target ${uploadTarget.type}`);
   }
 
-  let [targetData] = data.uploadConfigs.filter(config => config.type === uploadTarget.type);
-  if (!targetData) targetData = {type: uploadTarget.type};
+  let [targetData] = data.uploadConfigs.filter((config) => config.type === uploadTarget.type);
+  if (!targetData) targetData = { type: uploadTarget.type };
 
   // quick hack to allow overwrite of inputDir and outputDir if using mm-preview
   targetData = {
     ...targetData,
-    ...options // options from commandline args could overwrite some of the keys in targetData like type, inputDir, outputDir
-  }
+    ...options, // options from commandline args could overwrite some of the keys in targetData like type, inputDir, outputDir
+  };
 
   targetData = await conditionalPrompt(targetData, {
     type: 'input',
@@ -144,22 +136,29 @@ module.exports = async (options = {}, cli) => {
   targetData = await conditionalPrompt(targetData, target.questions);
 
   // find and overwrite the correct object in the array data.uploadConfigs
-  const overwriteIndex = data.uploadConfigs.findIndex((config => config.type === targetData.type));
+  const overwriteIndex = data.uploadConfigs.findIndex((config) => config.type === targetData.type);
 
   if (overwriteIndex === -1) {
     //console.log("adding new object to data")
     data.uploadConfigs.push(targetData); //this config was not in the uploadrc yet so adding a new object
-  }
-  else {
+  } else {
     data.uploadConfigs[overwriteIndex] = targetData; //found it, so overwriting the existing object
   }
 
-  await fs.writeJSON(filepathRc, data, {spaces: 2})
+  await fs.writeJSON(filepathRc, data, { spaces: 2 });
 
   // console.log(targetData)
-  const start = Date.now()
+  const start = Date.now();
 
-  await target.action(targetData);
+  const result = await target.action(targetData);
+
+  // Save the build information for future comparisons
+  if (result) {
+    if (result.previousOutputDir) targetData.previousOutputDir = result.previousOutputDir;
+
+    // Save the updated configuration
+    await fs.writeJSON(filepathRc, data, { spaces: 2 });
+  }
 
   console.log(chalk.green(`Done in ${Date.now() - start}ms, Have a nice day!`));
 };
--- src/target/mm-preview.js
+++ src/target/mm-preview.js
@@ -3,12 +3,12 @@ const validateNotOutsideWorkingDir = require('../util/validate/validateNotOutsid
 const validateNotEmpty = require('../util/validate/validateNotEmpty');
 const { v4: uuidv4 } = require('uuid');
 const open = require('open');
-// const fs = require('fs-extra');
+const fs = require('fs-extra');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 const globPromise = require('glob-promise');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const { NodeHttpHandler } = require("@aws-sdk/node-http-handler");
+const { NodeHttpHandler } = require('@aws-sdk/node-http-handler');
 const cliProgress = require('cli-progress');
 
 const mime = require('mime-types');
@@ -60,6 +60,15 @@ const preview = {
 
     validateActionInput(data, this.questions);
 
+    // folder name with newly builded banners
+    const currentBuildDir = data.inputDir; // build folder
+
+    // folder with previously builded banners
+    const prevBuildStorageDir = path.join(process.cwd(), '.prev-build-snapshot');
+
+    // checking if previous version exists
+    const hasPrevBuild = await fs.pathExists(prevBuildStorageDir);
+
     const client = new S3Client({
       region: 'us-east-1',
       credentials: {
@@ -69,43 +78,132 @@ const preview = {
       requestHandler: new NodeHttpHandler({
         socketTimeout: 3000,
         timeoutByRequestType: {
-          default: 3000
+          default: 3000,
         },
-        maxSockets: 200 // Increase from default 50
-      })
+        maxSockets: 200, // Increase from default 50
+      }),
     });
 
-    const allFiles = await globPromise(`${data.inputDir.replace(/\\/g, '/')}/**/*`);
+    // Get a list of all files in the current build
+    const allFiles = await globPromise(`${currentBuildDir.replace(/\\/g, '/')}/**/*`);
+    const fileList = (
+      await Promise.all(
+        allFiles.map(async (file) => {
+          if ((await fs.lstat(file)).isFile()) {
+            return path.relative(currentBuildDir, file).replace(/\\/g, '/');
+          }
+          return null;
+        }),
+      )
+    ).filter(Boolean);
 
-    const filesArray = (await Promise.all(allFiles.map(async file => {
-      if ((await fs.promises.lstat(file)).isFile()) {
-        return new PutObjectCommand({
-          Bucket: data.bucket,
-          Key: data.outputDir + path.relative(data.inputDir, file).replace(/\\/g, '/'),
-          ContentType: mime.lookup(file),
-          Body: await fs.promises.readFile(file),
-        })
-      }
-    })))
-    .filter(file => file !== undefined)
+    // Identify the modified files by comparing with the previous build
+    let changedFiles = [];
 
+    if (hasPrevBuild) {
+      console.log('Previous build found. Define modified files...');
+
+      changedFiles = await Promise.all(
+        fileList.map(async (relativePath) => {
+          //ignoring zip files
+          if (relativePath.endsWith('.zip')) {
+            return null;
+          }
+
+          const currentFile = path.join(currentBuildDir, relativePath);
+          const prevFile = path.join(prevBuildStorageDir, relativePath);
+
+          // Check if the file exists in the previous build
+          if (!(await fs.pathExists(prevFile))) {
+            return relativePath; // new file
+          }
+
+          // Comparing file contents
+          const currentContent = await fs.readFile(currentFile);
+          const prevContent = await fs.readFile(prevFile);
+
+          if (!currentContent.equals(prevContent)) {
+            return relativePath; // Modified file
+          }
+
+          return null; // Unmodified file
+        }),
+      );
+
+      changedFiles = changedFiles.filter(Boolean);
+      console.log(`Found ${changedFiles.length} of changed files from ${fileList.length}`);
+    } else {
+      console.log('Previous build not found. Download all files...');
+      changedFiles = fileList; // All files are considered modified
+    }
+
+    // If there are no changes, open an existing preview
+    if (changedFiles.length === 0 && data.previousOutputDir) {
+      console.log('No changes detected. Open an existing preview.');
+      open(`http://${data.bucket}.s3.amazonaws.com/${data.previousOutputDir}index.html`);
+      return {
+        previousOutputDir: data.previousOutputDir,
+      };
+    }
+
+    console.log('edited files: ', changedFiles);
+
+    // Uploading files to the S3
+    const filesToUpload = changedFiles.length > 0 ? changedFiles : fileList;
     const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    progressBar.start(filesArray.length, 0);
+    progressBar.start(filesToUpload.length, 0);
 
-    await Promise.all(
-      filesArray.map(async file => {
-        try {
-          await client.send(file);
-        } catch (e) {
-          console.log(e);
-        }
-        progressBar.increment();
-      }),
-    );
+    // Use batch loading to improve performance
+    const BATCH_SIZE = 50;
+    for (let i = 0; i < filesToUpload.length; i += BATCH_SIZE) {
+      const batch = filesToUpload.slice(i, i + BATCH_SIZE);
+
+      await Promise.all(
+        batch.map(async (relativePath) => {
+          const fullPath = path.join(data.inputDir, relativePath);
+          try {
+            await client.send(
+              new PutObjectCommand({
+                Bucket: data.bucket,
+                Key: data.outputDir + relativePath,
+                ContentType: mime.lookup(fullPath),
+                Body: await fs.readFile(fullPath),
+              }),
+            );
+          } catch (e) {
+            console.log(`Error during download ${relativePath}:`, e);
+          }
+          progressBar.increment();
+        }),
+      );
+    }
 
     progressBar.stop();
+
+    // After successful download, save the current build (without zip files) as the previous one
+    console.log('Saving the current build for future comparisons...');
+    await fs.emptyDir(prevBuildStorageDir);
+    // await fs.copy(currentBuildDir, prevBuildStorageDir);
+
+    await fs.copy(currentBuildDir, prevBuildStorageDir, {
+      filter: (src, dest) => {
+        // ignoring ZIP-files
+        if (src.toLowerCase().endsWith('.zip')) {
+          return false;
+        }
+
+        // copy all other files
+        return true;
+      },
+    });
+
     console.log(`go to http://${data.bucket}.s3.amazonaws.com/${data.outputDir}index.html`);
     open(`http://${data.bucket}.s3.amazonaws.com/${data.outputDir}index.html`);
+
+    // Return the information to be saved in .uploadrc
+    return {
+      previousOutputDir: data.outputDir,
+    };
   },
 };
 
